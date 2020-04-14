0a35e63bb450970d6b41eb7c693363ca203415d3
Raymond Fallon <git@niclic.com>
2020-04-14 17:35:26 -0400

    Avoid using the DefaultServeMux.

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -7,13 +7,14 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	router := http.NewServeMux()
+	router.HandleFunc("/", helloHandler)
 
-	log.Println("Listening on port 8080...")
+	log.Println("Listening on port 8080...\n")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!\n")
 }
