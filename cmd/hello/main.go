0e99a10bfa6d13f00a8fbf5cde4aed2a59b0080e
Raymond Fallon <git@niclic.com>
2020-04-15 14:12:09 -0400

    Extract some basic functions to simplify main.

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -6,15 +6,23 @@ import (
 	"net/http"
 )
 
-func main() {
+func createHttpRouter() *http.ServeMux {
 	router := http.NewServeMux()
 	router.HandleFunc("/", helloHandler)
+	return router
+}
 
-	log.Println("Listening on port 8080...\n")
-
-	log.Fatal(http.ListenAndServe(":8080", router))
+func startServer(port int, router *http.ServeMux) {
+	log.Printf("Listening on port %d...\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!\n")
+}
+
+func main() {
+	defaultPort := 8080
+	router := createHttpRouter()
+	startServer(defaultPort, router)
 }
